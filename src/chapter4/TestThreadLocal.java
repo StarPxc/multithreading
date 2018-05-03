458b3af7c748a408f90f94d410adb80d33ce3ba6
starPxc <2955317305@qq.com>
2018-05-03 15:08:28 +0800

    2018/5/3

--- src/chapter4/TestThreadLocal.java
+++ src/chapter4/TestThreadLocal.java
@@ -47,6 +47,8 @@ public class TestThreadLocal {
          * 为每一个线程人手分配一个对象的工作并不是由ThreadLocal来完成的，而是需要在应用层面保证的。
          * 如果在应用上为每一个线程分配了相同的对象实例，那么ThreadLocal也不能保证线程安全。
          * ThreadLocal底层是一个ThreadLocalMap，这是一个弱引用，JVM在垃圾回收时，如果发现弱引用，就会立即回收
+         * 如果你希望及时回收放在ThreadLocal中的对象，最好使用ThreadLocal.remove()方法，因为当对象太大且当前线程
+         * 放在线程池中的时候可能会造成内存泄漏。
          */
     }
 }
